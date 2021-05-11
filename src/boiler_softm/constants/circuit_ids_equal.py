becbd92ae7feead626fd2496ac791d7d563a0078
Georgij <netbay.georgij@gmail.com>
2021-05-12 00:55:49 +0500

    CIRUCIT_TYPES!!!!

--- src/boiler_softm/constants/circuit_ids_equal.py
+++ src/boiler_softm/constants/circuit_ids_equal.py
@@ -1,7 +1,7 @@
 from boiler_softm.constants import circuit_ids as soft_m_circuit_ids
-from boiler.constants import circuit_ids
+from boiler.constants import circuit_types
 
 CIRCUIT_ID_EQUALS = {
-    soft_m_circuit_ids.HOT_WATER_CIRCUIT: circuit_ids.HOT_WATER_CIRCUIT,
-    soft_m_circuit_ids.HEATING_CIRCUIT: circuit_ids.HEATING_CIRCUIT
+    soft_m_circuit_ids.HOT_WATER_CIRCUIT: circuit_types.HOT_WATER,
+    soft_m_circuit_ids.HEATING_CIRCUIT: circuit_types.HEATING
 }
