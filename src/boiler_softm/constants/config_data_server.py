d1794bee42edf5f08a4479917b6afd2030dcf7bb
Ruslan <bannikov.ru@inbox.ru>
2022-02-03 19:49:02 +0500

    Внесены изменения для получения данных по Черной Ушке

---
+++ src/boiler_softm/constants/config_data_server.py
@@ -0,0 +1,9 @@
+# Lysva
+# DATA_SERVER = "https://lysva.agt.town"
+# GET_TEMP_GRAPH_METHOD = "getTempGraphic"
+# GET_WEATHER_FORECAST_METHOD = "getPrognozT"
+
+# Chernushka
+DATA_SERVER = "https://chern.agt.town/json"
+GET_TEMP_GRAPH_METHOD = "ai_getTempGraphic"
+GET_WEATHER_FORECAST_METHOD = "ai_getPrognozT"
